4a487b0e77ca9d69c0e3faf28db8d0624384c0dc
valentine <yecvhuk.valentine@gmaiil.com>
2023-10-12 20:14:35 +0200

    finish_jQuery1

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $('#slide1, #slide2, #slide3, #slide4, #slide5, #slide6, #slide7, #slide8').on('
     // $('#output').css('filter',slide1 + '% ' + slide2 + '% ' + slide3 + '% ' + slide4 + '% ' + slide5 + '% ' + slide6 + '% ' + slide7 + '% ' + slide8 + '% ');
     $('#output').css('filter', `grayscale(${slide1}%) sepia(${slide2}%) blur(${slide3}px) invert(${slide4}%) opacity(${slide5}%) brightness(${slide6}%) contrast(${slide7}%) saturate(${slide8}%)`);
     // $('#box').val('filter: ' + slide1 + '% ' + slide2 + '% ' + slide3 + 'px ' + slide4 + '% ' + slide5 + '% ' + slide6 + '% ' + slide7 + '% ' + slide8 + '% ');
-    $('#box').val(`filter: grayscale(${slide1}%) sepia(${slide2}%) blur(${slide3}px) invert(${slide4}%) opacity(${slide5}%) brightness(${slide6}%) contrast(${slide7}%) saturate(${slide8}%)`);
+    $('#box').text(`filter: grayscale(${slide1}%) sepia(${slide2}%) blur(${slide3}px) invert(${slide4}%) opacity(${slide5}%) brightness(${slide6}%) contrast(${slide7}%) saturate(${slide8}%)`);
 
 
 
